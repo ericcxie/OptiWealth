c08fd7972224f4004aeacf1fa52972bc556f2b7d
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-09-25 20:00:38 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # OptiWealth
- 
+
+asdasd
---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-gray-100 h-screen flex justify-center items-center">
+      <div className="text-4xl font-semibold text-indigo-700">
+        Welcome to OptiWealth
+      </div>
+    </div>
+  );
+}
+
+export default App;
