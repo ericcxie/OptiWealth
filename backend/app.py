f6cef24c54d920381ff16c2551c7db0111ecf4b5
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-10-06 18:15:08 -0400

    Implement caching for portfolio data retrieval

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 import pandas as pd
 from flask_cors import CORS
+from flask_caching import Cache
 from dotenv import load_dotenv
 from sqlalchemy.exc import IntegrityError
 import logging
+from threading import Thread
 
 from app_functions import parse_image, clean_data, get_portfolio_data, get_stock_prices
 from models.models import UserPortfolio
@@ -14,17 +16,19 @@ from db.db import db
 
 app = Flask(__name__)
 load_dotenv()
-# print(os.getenv('DATABASE_URI'))
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db.init_app(app)
 CORS(app)
 
-# Path for uploaded files
+# Caching setup
+app.config["CACHE_TYPE"] = "SimpleCache"
+app.config["CACHE_DEFAULT_TIMEOUT"] = 300  # 5 minutes cache timeout
+cache = Cache(app)
+
 UPLOAD_FOLDER = 'data/uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'csv', 'xlsx'])
 
 
@@ -119,17 +123,34 @@ def submit_portfolio():
 @app.route('/get-portfolio-value', methods=['POST'])
 def get_portfolio_value():
     user_email = request.json.get('email')
-    portfolio_data = get_portfolio_data(user_email)
+    cache_key = f'portfolio_value_{user_email}'
 
+    cached_value = cache.get(cache_key)
+
+    if cached_value:
+        Thread(target=update_portfolio_cache,
+               args=(user_email, cache_key)).start()
+        return jsonify({'portfolio_value': cached_value})
+    else:
+        value = calculate_portfolio_value(user_email)
+        cache.set(cache_key, value)
+        return jsonify({'portfolio_value': value})
+
+
+def calculate_portfolio_value(user_email):
+    portfolio_data = get_portfolio_data(user_email)
     portfolio_dict = {stock['Ticker']: stock['Total Shares']
                       for stock in portfolio_data[0]}
-
     tickers = list(portfolio_dict.keys())
     stock_prices = get_stock_prices(tickers)
-
     total_value = sum(stock_prices[ticker] *
                       portfolio_dict[ticker] for ticker in tickers)
-    return jsonify({'portfolio_value': total_value})
+    return total_value
+
+
+def update_portfolio_cache(user_email, cache_key):
+    value = calculate_portfolio_value(user_email)
+    cache.set(cache_key, value)
 
 
 if __name__ == '__main__':
