85c7061a98df7d7a2a4a02f0f632b8c7b02fe181
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-11-08 11:21:50 -0500

    Feat: add row btn #2

--- client/src/components/tables/UploadPortfolioTable.tsx
+++ client/src/components/tables/UploadPortfolioTable.tsx
@@ -74,9 +74,9 @@ const UploadPortfolioTable: React.FC<PortfolioTableProps> = ({
   };
 
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <div className="relative overflow-x-auto overflow-y-auto h-[30rem] shadow-md sm:rounded-lg">
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-md text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <thead className="sticky top-0 text-md text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             <th scope="col" className="px-6 py-3">
               Ticker
