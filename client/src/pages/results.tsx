5529790dd709d347f8c8569820c5e91de285b11e
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-11-09 17:25:32 -0500

    Open link in new page

--- client/src/pages/results.tsx
+++ client/src/pages/results.tsx
@@ -146,6 +146,8 @@ const Results: React.FC = () => {
             </p>
             <a
               href="https://tinyurl.com/OptiWealthDocs"
+              target="_blank"
+              rel="noopener noreferrer"
               className="text-blue-500 hover:underline cursor-pointer mb-2"
             >
               Learn more about these results
