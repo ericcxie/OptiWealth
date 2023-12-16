be0f3432c6edfbc8024822d339f13ec34da703ab
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-12-15 22:45:37 -0500

    Update database configuration

--- backend/app/utils/app_functions.py
+++ backend/app/utils/app_functions.py
@@ -24,13 +24,15 @@ load_dotenv()
 DATABASE_NAME = os.getenv('DATABASE_NAME')
 DATABASE_USER = os.getenv('DATABASE_USER')
 DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_HOST = os.getenv('DATABASE_HOST')
+DATABASE_PORT = os.getenv('DATABASE_PORT')
 
 DATABASE_CONFIG = {
     'dbname': DATABASE_NAME,
     'user': DATABASE_USER,
     'password': DATABASE_PASSWORD,
-    'host': 'localhost',
-    'port': '5432'
+    'host': DATABASE_HOST,
+    'port': DATABASE_PORT
 }
 
 c = CurrencyRates()
