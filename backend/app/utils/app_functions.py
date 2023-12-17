cf7c34020e5afd67112364ebef9ec40efefe57cb
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-12-17 14:56:01 -0500

    Revert  database configuration in app_functions.py

--- backend/app/utils/app_functions.py
+++ backend/app/utils/app_functions.py
@@ -28,20 +28,12 @@ DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
 DATABASE_HOST = os.getenv('DATABASE_HOST')
 DATABASE_PORT = os.getenv('DATABASE_PORT')
 
-# DATABASE_CONFIG = {
-#     'dbname': DATABASE_NAME,
-#     'user': DATABASE_USER,
-#     'password': DATABASE_PASSWORD,
-#     'host': DATABASE_HOST,
-#     'port': DATABASE_PORT
-# }
-
 DATABASE_CONFIG = {
     'dbname': DATABASE_NAME,
     'user': DATABASE_USER,
     'password': DATABASE_PASSWORD,
-    'host': 'monorail.proxy.rlwy.net',
-    'port': '32065'
+    'host': DATABASE_HOST,
+    'port': DATABASE_PORT
 }
 
 c = CurrencyRates()
